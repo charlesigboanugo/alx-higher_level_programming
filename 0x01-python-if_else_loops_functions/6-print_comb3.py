be73228145b6bc3d6d42fca4e30991cda7f6f8be
charlesigboanugo <charlesigboanugo033@gmail.com>
2023-05-15 04:20:43 -0700

    test

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 89):
-    if i % 10 > i // 10:
-        print("{:02}".format(i), ", ", sep='', end='')
-print("89")
+for n in range(0, 10):
+    for m in range(n + 1, 10):
+        print("{}{}".format(n, m), end="")
+        if n != 8 and n != 9:
+            print(", ", end="")
+print()
