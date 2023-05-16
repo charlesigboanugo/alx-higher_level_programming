8193c978fee3a684e648ce4356fb88f535e3ef73
charlesigboanugo <charlesigboanugo033@gmail.com>
2023-05-15 17:05:27 -0700

    edited

--- 0x01-python-if_else_loops_functions/main.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-fizzbuzz = __import__('test').fizzbuzz
-fizzbuzz()
--- 0x01-python-if_else_loops_functions/test.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-for c in range(122, 96, -1):
-    letter = chr(c)
-    if c % 2 == 1:
-        letter = chr(c - 32)
-    print("{}".format(letter), end="")
