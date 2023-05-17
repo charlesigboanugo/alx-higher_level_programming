67fd50f844ef838d09d8121b5b076149b84db9fb
charlesigboanugo <charlesigboanugo033@gmail.com>
2023-05-17 08:59:29 +0000

    edit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-from sys import argv
 if __name__ == "__main__":
-    av = argv[1:]
-    avlen = len(av)
-    print("{} argument{}".format(avlen, "s" if avlen != 1 else ""), end="")
-    print("{}".format(":" if avlen else "."))
-    for i, v in enumerate(av):
-        print("{}: {}".format(i + 1, v))
+    from sys import argv
+    ac = len(argv) - 1
+    print("{} argument{}".format(ac, "s" if ac != 1 else ""), end="")
+    print("{}".format(":" if ac else "."))
+    for ind, arg in enumerate(argv[1:], 1):
+        print("{}: {}".format(ind, arg))
