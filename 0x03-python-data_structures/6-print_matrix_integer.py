0cf9be97a159aed3d50c2dd39c7e8d213f237791
charlesigboanugo <charlesigboanugo033@gmail.com>
2023-05-19 10:58:09 +0000

    yes

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         if row is not None:
             for x in range(0, len(row)):
-                print("{}".format(row[x]), end='')
+                print("{:d}".format(row[x]), end='')
                 if x is not len(row) - 1:
                     print(" ", end='')
         print()
--- 0x03-python-data_structures/test.py
+++ 0x03-python-data_structures/test.py
@@ -5,7 +5,8 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         if row is not None:
             for x in range(0, len(row)):
-                print("{}".format(row[x]), end='')
+                print("{:d}".format(row[x]), end='')
                 if x is not len(row) - 1:
                     print(" ", end='')
         print()
+print_matrix_integer([[1, 2, 3], None, [7, None, 9]])
