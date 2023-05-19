d1a7682897e28487ec9a824bfeb3fd8b5b38b855
charlesigboanugo <charlesigboanugo033@gmail.com>
2023-05-19 08:31:07 +0000

    updated

--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-replace_in_list = __import__('test').replace_in_list
+print_reversed_list_integer = __import__('test').print_reversed_list_integer
 
 my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = replace_in_list(my_list, idx, new_element)
-
-print(new_list)
-print(my_list)
+print_reversed_list_integer(my_list)
+my_list = None
+print_reversed_list_integer(my_list)
+my_list = [1]
+print_reversed_list_integer(my_list)
+my_list = []
+print_reversed_list_integer(my_list)
