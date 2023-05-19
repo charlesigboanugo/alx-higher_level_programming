bdec1591537134784da22f3c4bf13bc000b81ae8
charlesigboanugo <charlesigboanugo033@gmail.com>
2023-05-19 10:36:53 -0700

    finally

--- 0x03-python-data_structures/test.py
+++ 0x03-python-data_structures/test.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if tuple_a == None or tuple_b == None:
+    if tuple_a is None or tuple_b is None:
         return
     x = 0
-    if len(tuple_a) != 0 and tuple_a[0] != None:
+    if len(tuple_a) != 0 and tuple_a[0] is not None:
         x += tuple_a[0]
-    if len(tuple_b) != 0 and tuple_b[0] != None:
+    if len(tuple_b) != 0 and tuple_b[0] is not None:
         x += tuple_b[0]
     y = 0
-    if len(tuple_a) > 1 and tuple_a[1] != None:
+    if len(tuple_a) > 1 and tuple_a[1] is not None:
         y += tuple_a[1]
-    if len(tuple_b) > 1 and tuple_b[1] != None:
+    if len(tuple_b) > 1 and tuple_b[1] is not None:
         y += tuple_b[1]
     return x, y
-
