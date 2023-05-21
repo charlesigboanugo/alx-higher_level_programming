241e57dec3f70af8a79c6b3ea3318cffbce47998
charlesigboanugo <charlesigboanugo033@gmail.com>
2023-05-21 11:47:59 +0000

    an optimized version

--- 0x03-python-data_structures/test.c
+++ 0x03-python-data_structures/test.c
@@ -7,7 +7,6 @@
 *
 * Return: 0 if it is not a palindrome, 1 if it is a palindrome
 */
-
 int is_palindrome(listint_t **head)
 {
 	listint_t *ptr, *end;
@@ -21,13 +20,18 @@ int is_palindrome(listint_t **head)
 	for (len = 0; ptr != NULL; ptr = ptr->next)
 		len++;
 	ptr = *head;
-	for (i = 0; i < len / 2; i++, ptr = ptr->next)
+	end = ptr;
+	for (i = 0, j = 1; i < len / 2; j++, end = end->next)
 	{
-		end = ptr;
-		for (j = 1; j < len - 2 * (i); j++)
-			end = end->next;
-		if (ptr->n != end->n)
-			return (0);
+		if (j == len - 2 * (i))
+		{
+			if (ptr->n != end->n)
+				return (0);
+			ptr = ptr->next;
+			end = ptr;
+			i++;
+			j = 1;
+		}
 	}
 	return (1);
 }
