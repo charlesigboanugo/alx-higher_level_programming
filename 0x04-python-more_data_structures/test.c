42f97a95576c1e6aacac654b8b5f6a00878a495b
charlesigboanugo <charlesigboanugo033@gmail.com>
2023-06-04 08:28:50 +0000

    testing cpython

---
+++ 0x04-python-more_data_structures/test.c
@@ -0,0 +1,41 @@
+#include <Python.h>
+#include <stdio.h>
+
+void print_python_bytes(PyObject *p)
+{
+	PyBytesObject *bob = (PyBytesObject *) p;
+	Py_ssize_t bsize = PyBytes_Size(p), i;
+
+	printf("[.] bytes object info\n");
+	if (PyBytes_Check(p))
+	{
+		printf("  size: %ld\n", bsize);
+		printf("  trying string: %s", PyBytes_AsString(p));
+		for (i = 0; i <= bsize; i++)
+			printf("  first %ld bytes: %c\n", i, (bob->ob_sval)[i]);
+	}
+	else
+		printf("  [ERROR] Invalid Bytes Object");
+
+}
+
+void print_python_list(PyObject *p)
+{
+	PyListObject *lob = (PyListObject *) p;
+	Py_ssize_t lsize = PyList_Size(p), i;
+
+	printf("[.] Python list info\n");
+	if (PyList_Check(p))
+	{
+		printf("[*] Size of the Python List = %ld\n", lsize);
+		printf("[*] Allocated = %ld\n", lob->allocated);
+		for (i = 0; i < lsize; i++)
+		{
+			printf("Element %ld: %s\n", i, (lob->ob_item)[i]->tp_name);
+			if (PyBytes_Check(lob->ob_item[i]))
+				print_python_bytes(PyObject *p);
+		}
+	}
+	else
+		printf("[ERROR] Invalid List Object");
+}
