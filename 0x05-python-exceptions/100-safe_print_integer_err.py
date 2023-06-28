3754e7028c2b352bb2755237cfb7fcac5074622d
charlesigboanugo <charlesigboanugo033@gmail.com>
2023-06-28 06:29:52 -0700

    safe-int

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 import sys
 
-
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except BaseException as x:
+    except Exception as x:
         print("Exception: {}".format(x), file=sys.stderr)
         return False
     return True
--- 0x05-python-exceptions/test.py
+++ 0x05-python-exceptions/test.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
-def raise_exception():
-    raise TypeError()
+import sys
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except ValueError as va_err:
+        print("Exception: {}".format(va_err), file=sys.stderr)
+        return False
+    except TypeError as ty_err:
+        print("Exception: {}".format(ty_err), file=sys.stderr)
+        return False
+    return True
