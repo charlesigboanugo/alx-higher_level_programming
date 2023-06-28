d413bac4fb0384a1c0d23377e6b032cdaa5310a3
charlesigboanugo <charlesigboanugo033@gmail.com>
2023-06-28 05:33:03 -0700

    ex-4

--- 0x05-python-exceptions/test.py
+++ 0x05-python-exceptions/test.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
-
-def safe_print_integer(value):
-    try:
-        print("{:d}".format(value))
-    except ValueError:
-        return False
-    except TypeError:
-        return False
-    return True
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for x in range(list_length):
+        item = 0
+        try:
+            item = my_list_1[x] / my_list_2[x]
+        except TypeError:
+            print("wrong type")
+        except ValueError:
+            print("wrong type")
+        except ZeroDivisionError:
+            print("division by zero")
+        except IndexError:
+            print("out of range")
+        finally:
+            result.append(item)
+    return result
