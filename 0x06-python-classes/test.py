29d4c3bb2fac3854ecd5c7fa2619d0d86c1ec2e1
charlesigboanugo <charlesigboanugo033@gmail.com>
2023-06-30 11:14:29 +0000

    done 1

--- 0x06-python-classes/test.py
+++ 0x06-python-classes/test.py
@@ -5,5 +5,14 @@
 class square:
     """ A square class """
 
+    def __init__(self, size):
+        self.__size = size
 
-print(square.__dict__)
+    def get_size(self):
+        return self.__size
+
+
+my_square = square(2)
+print(my_square.__dict__)
+print(my_square.__doc__)
+print(my_square.get_size())
