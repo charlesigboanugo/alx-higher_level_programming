c7a4fd9b1e893a3582a63dfab48a0940f2b3ac46
charlesigboanugo <charlesigboanugo033@gmail.com>
2023-07-11 00:06:47 +0000

    update

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-"""Module contains function that adds two integer values"""
+"""Defines a function that adds two integers"""
 
 
 def add_integer(a, b=98):
-    """Adds two numbers as integers, second has defaut value of 98"""
-    if type(a) is not int and type(a) is not float:
+    """Adds two numbers as integers, b has a default value of 98"""
+    if a != a:
+        a = 89
+    if b != b:
+        b = 89
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
+    result = a + b
+    if result == float('inf') or result == -float('inf'):
+        return 89
     return int(a) + int(b)
