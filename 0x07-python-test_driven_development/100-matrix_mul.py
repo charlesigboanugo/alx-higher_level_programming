417db601a505a09be2c7fe038037c820c25cb7d2
charlesigboanugo <charlesigboanugo033@gmail.com>
2023-07-13 09:11:58 +0000

    check

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -8,15 +8,19 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_a must be a list")
     if type(m_b) is not list:
         raise TypeError("m_b must be a list")
+    if len(m_a) == 0:
+        raise TypeError("m_a must be a list of lists")
+    if len(m_b) == 0:
+        raise TypeError("m_b must be a list of lists")
     for row in m_a:
         if type(row) is not list:
             raise TypeError("m_a must be a list of lists")
     for row in m_b:
         if type(row) is not list:
             raise TypeError("m_b must be a list of lists")
-    if m_a == [] or m_a == [[]]:
+    if m_a == [[]]:
             raise ValueError("m_a can't be empty")
-    if m_b == [] or m_b == [[]]:
+    if m_b == [[]]:
             raise ValueError("m_b can't be empty")
     for row in m_a:
         for col in row:
