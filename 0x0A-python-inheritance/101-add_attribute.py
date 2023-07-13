a7f6d23e94f2ebff2cce61c1e0ffefdf32d52313
charlesigboanugo <charlesigboanugo033@gmail.com>
2023-07-13 16:36:42 +0000

    good

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -6,7 +6,4 @@ def add_attribute(obj, name, value):
     """A function that adds a new attribute to an object if it’s possible:"""
     if type(name) is not str:
         raise TypeError("can't add new attribute")
-    try:
-        exec("obj.{} = value".format(name))
-    except:
-        raise TypeError("can't add new attribute")
+    obj.name = value
