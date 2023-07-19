6bf754de1b3892396a56b8d579d36b18a27d2d95
charlesigboanugo <charlesigboanugo033@gmail.com>
2023-07-19 07:50:01 -0700

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """Makes a list from args and saves them to a file in json format"""
-
-
 import sys
 import json
-savejson = __import__("7-save_to_json_file").save_to_json_file
-loadjson = __import__("8-load_from_json_file").load_from_json_file
+savejson = __import__("5-save_to_json_file").save_to_json_file
+loadjson = __import__("6-load_from_json_file").load_from_json_file
 
 try:
     listy = loadjson("add_item.json")
