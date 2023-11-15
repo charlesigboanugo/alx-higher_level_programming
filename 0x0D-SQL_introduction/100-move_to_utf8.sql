ff73871fdaa23cb2fb87a1cbf1bcbc0d52fae608
charles igboanugo <charlesigboanugo033@gmail.com>
2023-11-15 07:45:11 +0100

    great

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,6 @@
 -- Convert the database character set abd collation
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
 
 -- convert first_table character set and collation
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
