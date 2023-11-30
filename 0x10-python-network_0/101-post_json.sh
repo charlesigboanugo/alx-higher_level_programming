62c50fe8cab6778b0c8e3d0a9d0b43432994f500
charles igboanugo <charlesigboanugo033@gmail.com>
2023-11-30 13:08:25 +0100

    good

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that sends a POST request with the content of a JSON file passed as a second argument to a URL passed as the first argument, and displays the body of the response.
-curl -X POST -H "Content-Type: application/json" -d "@$2" "$1"
+curl -s -X POST -H "Content-Type: application/json" -d "@$2" "$1"
