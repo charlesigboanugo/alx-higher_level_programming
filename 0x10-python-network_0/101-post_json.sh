38db24441216f24a5f35dade3a3a21bceb9b931d
charles igboanugo <charlesigboanugo033@gmail.com>
2023-11-30 13:03:25 +0100

    edit 101

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
-
-# Check if the required arguments are provided
-if [ "$#" -lt 2 ]; then
-    echo "Usage: $0 <URL> <filename>"
-    exit 1
-fi
-
-url=$1
-filename=$2
-
-# Check if the file exists
-if [ ! -f "$filename" ]; then
-    echo "Error: File not found - $filename"
-    exit 1
-fi
-
-# Send a JSON POST request with the file content in the body
-curl -X POST -H "Content-Type: application/json" -d "@$filename" "$url"
-
+# A script that sends a POST request with the content of a JSON file passed as a second argument to a URL passed as the first argument, and displays the body of the response.
+curl -X POST -H "Content-Type: application/json" -d "@$2" "$1"
