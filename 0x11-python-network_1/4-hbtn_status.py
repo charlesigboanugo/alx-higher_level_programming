4e84e2280e037e0e7e60018006efd650547df4f0
charles igboanugo <charlesigboanugo033@gmail.com>
2023-12-02 05:42:29 +0100

    crwate network files

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,7 +3,7 @@
 if __name__ == '__main__':
     import requests
 
-    response = requests.get('https://intranet.hbtn.io/status')
+    response = requests.get('https://alx-intranet.hbtn.io/status')
     print('Body response:')
     print('\t- type: {}'.format(type(response.text)))
     print('\t- content: {}'.format(response.text))
